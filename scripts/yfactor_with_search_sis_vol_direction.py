9a67fd0a8bb4b1a64f1d595c48c32e5be04db745
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-08-31 11:28:02 +0900

    asdfasdfgasdgasdgadg

--- scripts/yfactor_with_search_sis_vol_direction.py
+++ scripts/yfactor_with_search_sis_vol_direction.py
@@ -29,8 +29,8 @@ vol = np.linspace(0, 1.5, 300)   #search optimal SIS voltage value
 
 
 date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
-file_name_hot = name  + date +'/hot/' + date + '.necstdb'
-file_name_cold = name + date +'/cold/' + date + '.necstdb'
+file_name_hot = name  + '/'+date +'/hot/' + date + '.necstdb'
+file_name_cold = name + '/'+date +'/cold/' + date + '.necstdb'
 print(file_name_hot)
 print(file_name_cold)
 
