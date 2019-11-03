28a5cbac857882dbcccd0c80dabf7b046c31c8b1
kotakayoshi <tatuyuki.takashima@gmail.com>
2019-11-03 13:18:59 +0000

    add sis-iv

--- scripts/sis_i-v_curve_direction.py
+++ scripts/sis_i-v_curve_direction.py
@@ -24,13 +24,13 @@ date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
 file_name = name + '/' + date + '.necstdb'
 print(file_name)
 
-sis_vgap = numpy.arange(-1.5, 1.5, 0.001)
+sis_vgap = numpy.arange(-1.5, 1.5, 0.01)
 sis.set_vgap(-1.5)
 time.sleep(3)
 logger.start(file_name)
 for vgap in sis_vgap:
     sis.set_vgap(vgap)
-    time.sleep(0.03)
+    time.sleep(0.2)
     continue
 logger.stop()
 sis.set_vgap(0)
